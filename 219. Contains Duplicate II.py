9f958038d8e485f040b6fe568c7bc1769be42be8
leakey1905 <leakey1905@users.noreply.github.com>
2016-08-25 05:00:04 -0700

    Update 219. Contains Duplicate II.py
    Optimize the code

--- 219. Contains Duplicate II.py
+++ 219. Contains Duplicate II.py
@@ -5,24 +5,21 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        if k == 0:
+        lenn = len(nums)
+        if k == 0 or lenn == 0:
             return False
-        elif nums == []:
-            return False
+        elif lenn <= k:
+            return len(set(nums)) < len(nums)
         else:
-            i = 0
-            lenn = len(nums)
-            if k >= lenn:
-                return len(set(nums)) < len(nums)
-            else:
-                A = set(nums[0:k+1])
-                if len(A) < k+1:
+            A = set(nums[0:k+1])
+            if len(A) <= k:
+                return True
+            endi = lenn-k-1
+            j = k+1 # pointer to the new number
+            for i in range(endi):
+                A.remove(nums[i])
+                A.add(nums[j])
+                j += 1 #update the pointer
+                if len(A) <= k:
                     return True
-                for i in range(lenn-k-1):
-                    A.remove(nums[i])
-                    A.add(nums[i+k+1])
-                    if len(A) < k+1:
-                        return True
-                return False
-                
-                
+            return False
