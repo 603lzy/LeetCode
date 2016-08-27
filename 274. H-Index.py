5410e89d517e17d637aae8269d37b309abf5b4b7
leakey1905 <leakey1905@users.noreply.github.com>
2016-08-26 20:00:37 -0700

    Update 274. H-Index.py

--- 274. H-Index.py
+++ 274. H-Index.py
@@ -6,11 +6,10 @@ class Solution(object):
         :https://discuss.leetcode.com/topic/23352/a-python-solution
         """
         citations.sort(reverse = True)
-        N = len(citations)
         try:
             h = 0
             while not citations[h] <= h:
                 h += 1
             return h
         except IndexError:
-            return N
+            return len(citations)
