ee9390fe8e77cacb12e4e2ccb7bd836d3f70d528
leakey1905 <leakey1905@users.noreply.github.com>
2016-09-09 10:04:54 +0800

    Create 74. Search a 2D Matrix.py

---
+++ 74. Search a 2D Matrix.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for i in matrix:
+            if target in i:
+                return True
+        return False
