678dbc68f00f68d0118c615053a17937de4239b3
leakey1905 <leakey1905@users.noreply.github.com>
2016-09-10 10:47:48 +0800

    Create 47. Permutations II.py

---
+++ 47. Permutations II.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(set(itertools.permutations(nums)))
+        
