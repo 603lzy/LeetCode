60a5025dc22ae6db0e4cc487cd2029255f0c5607
leakey1905 <leakey1905@users.noreply.github.com>
2016-10-15 23:57:11 +0800

    Opitmize the code

--- 78. Subsets.py
+++ 78. Subsets.py
@@ -5,16 +5,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        A = [[]]
+        A = [[]]  # init return list, including the empty subset
         lenn = len(nums)
-        if lenn == 0:
-            return [[]]
-        elif lenn == 1:
-            return [[], nums]
-        else:
-            A.append(nums)
+        if lenn >= 1:
+            A.append(nums)  # nums contains more than one element
+        if lenn >= 2:
             for i in nums:
-                A.append([i])
-            for i in xrange(2, lenn):
-                A.extend([list(x) for x in itertools.combinations(nums, i)])
-            return A
+                A.append([i])  # convert single element to list
+            for j in xrange(2, lenn):
+                A.extend([list(x) for x in itertools.combinations(nums, j)])  # consider subsets with 2 to lenn-1 elements
+        return A
