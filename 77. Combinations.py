50165cd808619498b1adbbe892bd9ca2cc359a90
leakey <leakeythu@163.com>
2016-10-20 12:32:33 +0800

    Use itertools

---
+++ 77. Combinations.py
@@ -0,0 +1,9 @@
+import itertools
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        return [list(i) for i in itertools.combinations(range(1, n+1), k)]
