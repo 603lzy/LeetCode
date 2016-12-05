85118ff9844a6be724607e1c0a6508805212d2ef
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-05 23:01:46 +0800

    Create 9. Palindrome Number.c

---
+++ 9. Palindrome Number.c
@@ -0,0 +1,10 @@
+bool isPalindrome(int x) {
+    if (x < 0)
+        return 0;
+    int sum = 0, temp = x;
+    while (x){
+        sum = sum * 10 + x % 10;
+        x /= 10;
+    }
+    return temp == sum;
+}
