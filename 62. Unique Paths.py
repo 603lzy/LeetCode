9680d667353379318d95985f07b402043b7bbe21
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-06 09:15:00 +0800

    DP for Unique Path

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -1,27 +1,23 @@
 class Solution(object):
-    def countOfPath(self, m, n, i, j, cnt):
-        """
-        type i: int current row position
-        type j: int current col position
-        type cnt: int current path count to point(i, j)
-        
-        """
-        curcnt = cnt
-        if i != 0 and j != 0:
-            return self.countOfPath(m, n, i - 1, j, curcnt) + self.countOfPath(m, n, i, j - 1, curcnt)
-        elif i == 0 and j != 0:
-            return self.countOfPath(m, n, i, j - 1, curcnt)
-        elif i != 0 and j == 0:
-            return self.countOfPath(m, n, i - 1, j, curcnt)
-        if i == 0 and j == 0:
-            return cnt
-        
     def uniquePaths(self, m, n):
         """
         :type m: int
         :type n: int
         :rtype: int
+        https://discuss.leetcode.com/topic/67900/dp-solution-in-python
+        # basic dynamic programming
         """
-        cnt = self.countOfPath(m, n, m - 1, n - 1, 0)
-        return cnt
-        
+        if m > 1 and n > 1:
+            grid = [[0 for col in xrange(n)] for row in xrange(m)]
+            for row in xrange(1, m):
+                grid[row][0] = 1
+            for col in xrange(1, n):
+                grid[0][col] = 1 
+            for row in xrange(1, m):
+                for col in xrange(1, n):
+                    grid[row][col] = grid[row][col - 1] + grid[row - 1][col]
+            return grid[m - 1][n - 1]
+        elif not m or not n:
+            return 0
+        else: # m = 1 or n = 1
+            return 1
