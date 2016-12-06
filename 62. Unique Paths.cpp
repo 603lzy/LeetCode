090aa38eabbcda2af2083a3c96ec2de3c761413b
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-06 09:25:13 +0800

    Create 62. Unique Paths.cpp

---
+++ 62. Unique Paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+            int grid[m][n];
+            int i, j;
+            grid[0][0] = 0;
+            if (m > 1 && n > 1){
+                for (i = 1; i < m; i++)
+                    grid[i][0] = 1;
+                for (j = 1; j < n; j++)
+                    grid[0][j] = 1;
+                for (i = 1; i < m; i++)
+                    for (j = 1; j < n; j++)
+                        grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+                return grid[m - 1][n - 1];
+            }else
+                return !(!m || !n);// m or n = 0, no path; m or n = 1, only 1 path   
+    }
+};
