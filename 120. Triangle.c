246be01b1c3e8685d18c6af221f1b6f7abddbc63
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-06 21:05:17 +0800

    A variance of Python version.

---
+++ 120. Triangle.c
@@ -0,0 +1,31 @@
+int minimumTotal(int** triangle, int triangleRowSize, int *triangleColSizes) {
+    int** sumPath;
+    int r, c, m = triangleRowSize;
+    int min;
+    sumPath = (int **) malloc(sizeof(int*) * m);
+    for (r = 0; r < m; r++)
+        sumPath[r] = (int*) malloc(sizeof(int) * *(triangleColSizes + r));
+    for (r = 0; r < m; r++)
+        for (c = 0; c < *(triangleColSizes + r); c++)
+            sumPath[r][c] = 0;
+    sumPath[0][0] = triangle[0][0];
+    for (r = 1; r < m; r++)
+        for (c = 0; c < *(triangleColSizes + r); c++){
+            if (c == 0){
+                sumPath[r][c] = sumPath[r - 1][c] + triangle[r][c];
+            }else if (c == *(triangleColSizes + r) - 1){
+                sumPath[r][c] = sumPath[r - 1][c - 1] + triangle[r][c];
+            }
+            else{
+                if (sumPath[r - 1][c - 1] > sumPath[r - 1][c])
+                    sumPath[r][c] = triangle[r][c] + sumPath[r - 1][c];
+                else
+                    sumPath[r][c] = triangle[r][c] + sumPath[r - 1][c - 1];
+            }
+        }
+    min = sumPath[m - 1][0];
+    for (c = 0; c < *(triangleColSizes + m - 1); c++)
+        if (min > sumPath[m - 1][c])
+            min = sumPath[m - 1][c];
+    return min;
+}
