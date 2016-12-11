1fad7b8dc4f76decc9445a8fea21a9ebae0b36ef
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-11 18:38:50 +0800

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -5,11 +5,4 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        ls = list(s)
-        lt = list(t)
-        ls.sort()
-        lt.sort()
-        if ls == lt:
-            return True
-        else:
-            return False
+        return sorted(s) == sorted(t)
