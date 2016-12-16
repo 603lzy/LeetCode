57199f17f06df52174aeb04c8f3240e395211e1f
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-16 15:34:01 +0800

    Create 13. Roman to Integer.cpp

---
+++ 13. Roman to Integer.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int romanToInt(string s) {//use hash table
+        unordered_map<char, int> dict = {{'I', 1},{'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}};
+        int num = dict[s.back()], i = s.length() - 1;
+        while (--i >= 0)
+            num = num + (dict[s[i]] < dict[s[i + 1]] ? -dict[s[i]] : dict[s[i]]);
+        return num;
+    }
+};
