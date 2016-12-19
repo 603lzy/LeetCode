1259b32106954f77e675a187f2a70e305c0b27b9
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-19 10:11:58 +0800

    Create 461. Hamming Distance.py

---
+++ 461. Hamming Distance.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return bin(x^y).count("1") # convert to ob ^, then calculate the number of 1
