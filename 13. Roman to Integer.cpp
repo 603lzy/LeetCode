74e441a4fe9f4828691bceb412067f69734e90d3
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-22 08:25:04 +0800

    Update 13. Roman to Integer.cpp
    Fix bugs: check null string

--- 13. Roman to Integer.cpp
+++ 13. Roman to Integer.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int romanToInt(string s) {//use hash table
+        if (!s.length())
+            return 0; //check null string
         unordered_map<char, int> dict = {{'I', 1},{'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}};
         int num = dict[s.back()], i = s.length() - 1;
         while (--i >= 0)
