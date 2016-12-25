fa03688143628cf9400f0b1a51513f71803fc831
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-26 01:45:55 +0800

    Optimize the code

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,106 +1,21 @@
 class Solution(object):
-    def small(self, s, i, A):
-        """
-        :type s: str
-        :type i: int
-        :rtype: list
-        """
-        i = i + 1
-        if i == len(s):
-            return [False, i]
-        elif s[i] == ')':
-            i = i + 1
-            return [True, i]
-        elif s[i] == ']':
-            return [False, i]
-        elif s[i] == '}':
-            return [False, i]
-        elif s[i] == '(':
-            return self.small(s, i)
-        elif s[i] == '[':
-            return self.middle(s, i)
-        elif s[i] == '{':
-            return self.big(s, i)
-        else:
-            return self.small(s, i)
-    
-    def middle(self, s, i):
-        """
-        :type s: str
-        :type i: int
-        :rtype: bool
-        """
-        i = i + 1
-        if i == len(s):
-            return [False, i]
-        elif s[i] == ')':
-            return [False, i]
-        elif s[i] == ']':
-            i = i + 1
-            return [True, i]
-        elif s[i] == '}':
-            return [False, i]
-        elif s[i] == '(':
-            return self.small(s, i)
-        elif s[i] == '[':
-            return self.middle(s, i)
-        elif s[i] == '{':
-            return self.big(s, i)
-        else:
-            return self.middle(s, i)
-
-    def big(self, s, i):
-        """
-        :type s: str
-        :type i: int
-        :rtype: bool
-        """
-        i = i + 1
-        if i == len(s):
-            return [False, i]
-        elif s[i] == ')':
-            return [False, i]
-        elif s[i] == ']':
-            return [False, i]
-        elif s[i] == '}':
-            i = i + 1
-            return [True, i]
-        elif s[i] == '(':
-            return self.small(s, i)
-        elif s[i] == '[':
-            return self.middle(s, i)
-        elif s[i] == '{':
-            return self.big(s, i)
-        else:
-            return self.big(s, i)
-            
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        if len(s) <= 1:
+        if not s or len(s) % 2 == 1: # if the length is shoter than 1 or odd length
             return False
-        i = 0
-        sign = True
-        while i != len(s):
-            if s[i] == ')':
+        A = []
+        B = ['(', '[', '{']
+        C = [')', ']', '}']
+        for i in s:
+            if i in B:
+                A.append(i)
+            elif not A:
                 return False
-            elif s[i] == ']':
-                return False
-            elif s[i] == '}':
-                return  False
-            elif s[i] == '(':   
-                sign = self.small(s, i)
-            elif s[i] == '[':
-                sign = self.middle(s, i)
-            elif s[i] == '{':
-                sign = self.big(s, i)
+            elif A[-1] == B[C.index(i)]:
+                A.pop()
             else:
-                sign = [True, i+1]
-            i = sign[1]
-            print sign
-
-        return sign[0]
-            
-        
+                return False
+        return A == []
