0674236913998da6dcf5d01d8ca0ea0ef3d93e1b
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-26 00:48:32 +0800

    Optimize the code

--- 24. Swap Nodes in Pairs.py
+++ 24. Swap Nodes in Pairs.py
@@ -10,36 +10,21 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        lenh = 0
-        l1 = head
-        if head == None:
-            return None
-        elif head.next ==  None:
+        if not head or not head.next:
             return head
-        else:
-            D = head
-            G = head
-            S = head.next
-            T = head.next.next
-            G.next = T
-            S.next = G
-            D = S
-            head = D
-            while(G != None) and (S != None) and (T != None):
-                if S.next.next.next == None:
-                    break
-                else:
-                    T = T.next.next
-                    S = S.next.next.next
-                    G = G.next
-                    D = D.next
-                    D.next = None
-                    G.next = T
-                    S.next = G
-                    D.next = S
-                    D = D.next
-            return head
-                    
-        
-            
-            
+        D, G, S, T = head, head, head.next, head.next.next
+        G.next = T
+        S.next = G
+        D = S
+        head = D
+        while(G != None) and (S != None) and (T != None):
+            if S.next.next.next:
+                T, S, G, D = T.next.next, S.next.next.next, G.next, D.next
+                D.next = None
+                G.next = T
+                S.next = G
+                D.next = S
+                D = D.next
+            else:
+                break
+        return head
