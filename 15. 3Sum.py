246c01c004b2b2a100468ff648e671889e607112
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-28 01:25:09 +0800

    Optimize the code

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,32 +1,13 @@
 class Solution(object):
-    def twoSum(self, num, target, C, lennum):
-        j = lennum
-        a = 0
-        b = 0
-        while 0 != j:
-            a = num[j]
-            for k in range(0, j):
-                b = num[k]
-                if a + b == target:
-                    C.add((-target, b, a))
-            j = j - 1
-        return C
-                    
     def threeSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
-        """
+        """ 
         nums.sort()
-        C = set()
-        i = 0
-        tmp = len(nums) - 2
-        while i < tmp:
-            i = i + 1
-            lennum = tmp - i + 1
-            self.twoSum(nums[i:], -nums[i-1], C, lennum)
-            
-        C = list(C)
-        C.sort()
-        return C
-        
+        C, maxi = set(), len(nums) - 2 # maxi: max index of pointer i backward
+        for i in xrange(maxi):
+            for j in xrange(i + 1, maxi + 1):
+                if -nums[i] - nums[j] in nums[j + 1: ]:
+                    C.add((nums[i], nums[j], -nums[i] - nums[j]))
+        return list(C)
