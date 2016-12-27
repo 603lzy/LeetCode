6a8d675d37da2290d244e80e7f45dac178e69550
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-27 22:59:03 +0800

    Optimize the code

--- 22. Generate Parentheses.py
+++ 22. Generate Parentheses.py
@@ -4,37 +4,27 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        i = 0
-        output = 2*n
-        B = [i for i in range(n)]
+        B = [i for i in xrange(n)]
         p = ""
-        for k in range(0, output):
-            if k in B:
-                p = p + "("
-            else:
-                p = p + ")"
+        for k in xrange(0, 2 * n):
+            p += "(" if k in B else ")"
         A = [p]
-        i = n-1
-        j = i
+        j = i = n - 1
         while True:
-            if B[j] != 2*j: # adjustment b
+            if B[j] != 2 * j: # adjustment b
                 B[j] += 1
             else:
                 while j != 0:
                     j -= 1
-                    if B[j] != 2*j:
+                    if B[j] != 2 * j:
                         B[j] += 1
-                        for m in range(j+1, n):
-                            B[m] = B[m-1] + 1
-                        j = n-1
+                        for m in xrange(j + 1, n):
+                            B[m] = B[m - 1] + 1
+                        j = n - 1
                         break
                 if j <= 0:
                     return A
-                
             p = "" # then print
-            for k in range(0, output):
-                if k in B:
-                    p = p + "("
-                else:
-                    p = p + ")"
+            for k in xrange(0, 2 * n):
+                p += "(" if k in B else ")"
             A.append(p)
