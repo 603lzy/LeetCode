f12caa2c925b35c4d08372288e40e4ae2bb1cfea
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-27 17:42:59 +0800

    Optimize the code

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -4,24 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        j = 0
-        l = 0
-        sign = 0
-        print(len(s))
-        if len(s) == 0:
-            return 0
-        if len(s) == 1:
-            return 1
-        j = 1
-        i = 0
+        sLen = len(s)
+        if sLen <= 1: return sLen  # s is shorter than 2 letters
+        subStrLen, i, j = 0, 0, 1
         L = [1]
-        while (i < len(s) and i + L[i - 1] < len(s) and L[i - 1] < 95):
-            for j in range(1, len(s)):
+        while j < sLen and L[i - 1] < 95:
+            for j in xrange(i, sLen):
                 if s[j] in s[i:j]:
                     break
                 else:
-                    j = j + 1
+                    j += 1
             L.append(j - i)
-            i = i + 1
-            l = max(L)
-        return l
+            i += 1
+            subStrLen = max(L[-1], subStrLen)
+        return subStrLen
