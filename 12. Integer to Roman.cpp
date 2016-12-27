d90e0ca04a6de9bebdf37ef5f8ca222922c09851
leakey1905 <leakey1905@users.noreply.github.com>
2016-12-27 23:56:20 +0800

    Optimize code & add comments

--- 12. Integer to Roman.cpp
+++ 12. Integer to Roman.cpp
@@ -1,20 +1,15 @@
 class Solution {
-public:
+public: // Use dict to convert 0 - 3999 to Roman
     string intToRoman(int num) {
-        char* rom3[4] = {"", "M", "MM", "MMM"};
+        char* rom3[4] = {"", "M", "MM", "MMM"};                                        //0XXX - 3XXX
         char* rom2[10] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}; //0XX - 9XX
         char* rom1[10] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}; //0X - 9X
         char* rom0[10] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}; //0 - 9
-        int d[4];
-        d[0] = num % 10;
-        d[1] = num % 100 / 10;
-        d[2] = num % 1000 / 100;
-        d[3] = num / 1000;
         string s;
-        s += rom3[d[3]];
-        s += rom2[d[2]];
-        s += rom1[d[1]];
-        s += rom0[d[0]];
+        s += rom3[num / 1000];  //X...
+        s += rom2[num % 1000 / 100];  //.X..
+        s += rom1[num % 100 / 10];  //..X.
+        s += rom0[num % 10];  //...X
         return s;
     }
 };
