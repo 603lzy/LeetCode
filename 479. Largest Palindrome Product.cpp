adecc56347221c75ea1b5fb73c64f490183d697f
leakey1905 <leakey1905@users.noreply.github.com>
2017-01-04 18:38:30 +0800

    Create 479. Largest Palindrome Product.cpp

---
+++ 479. Largest Palindrome Product.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(long long int x) {
+    if (x < 0)
+        return 0;
+    int sum = 0, temp = x;
+    while (x){
+        sum = sum * 10 + x % 10;
+        x /= 10;
+    }
+    return temp == sum;
+}
+
+    int largestPalindrome(int n) {
+          switch(n){
+        case 5: return 677; // int cannot save that large number
+        case 6: return 1218;
+        case 7: return 877;
+        case 8: return 475;
+    }
+    long long int num = pow(10, n) - 1, i = 0, x, curnum;
+    while (true){
+        for (x = i; x >= 1; x--){
+            curnum = (num - x) * (num + x);
+            if (isPalindrome(curnum))
+                return curnum % 1337;
+        }
+        i++;
+        num--;
+}  
+    }
+};
