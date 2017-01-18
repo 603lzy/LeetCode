bb9c0442db47788f3dfe389a23458f50874faa11
leakey1905 <leakey1905@users.noreply.github.com>
2017-01-18 10:56:36 +0800

    Opitimize the code

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -4,13 +4,8 @@ class Solution(object):
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: float
-        """
-        
-        m = len(nums1)
-        n = len(nums2)
+        """  
+        m, n = len(nums1), len(nums2)
         nums = nums1 + nums2
         nums.sort()
-        if (m + n) % 2 == 0:
-            return (nums[(m + n)/2]+nums[(m + n)/2-1])/2.0
-        else:
-            return nums[(m + n - 1)/2]
+        return (nums[(m + n)/2]+nums[(m + n)/2 - 1])/2.0 if not (n + m) % 2 else nums[(m + n - 1)/2]
