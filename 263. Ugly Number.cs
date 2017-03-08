67f73e4f7737575dee56479eb5279c549b1820da
leakey1905 <leakey1905@users.noreply.github.com>
2017-03-08 09:47:20 +0800

    Create 263. Ugly Number.cs
    same code for c, c++, c#, java

---
+++ 263. Ugly Number.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool IsUgly(int num) {
+          if (num == 0)
+        return false;
+    if (num == 1)
+        return true;
+    else{
+        while (num % 2 == 0)
+            num /= 2;
+        while (num % 3 == 0)
+            num /= 3;
+        while (num % 5 == 0)
+            num /= 5;
+        if (num == 1)
+            return true;
+        else
+            return false;
+    }
+    }
+}
