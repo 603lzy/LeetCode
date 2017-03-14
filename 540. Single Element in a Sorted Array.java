1aa0c09dc6d3bd58033bd8195b91c2bc77b2edb6
leakey1905 <leakey1905@users.noreply.github.com>
2017-03-14 15:30:22 +0800

    Create 540. Single Element in a Sorted Array.java

---
+++ 540. Single Element in a Sorted Array.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i += 2){
+            if (nums[i] != nums[i + 1])
+                return nums[i];
+        }
+        return nums[nums.length - 1];
+    }
+}
