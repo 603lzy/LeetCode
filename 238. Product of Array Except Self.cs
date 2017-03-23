5b88167bcdb5125c5b668ce595dfd5b7d0d5e84a
leakey1905 <leakey1905@users.noreply.github.com>
2017-03-23 18:00:01 +0800

    Create 238. Product of Array Except Self.cs

---
+++ 238. Product of Array Except Self.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int l = nums.Length;
+        int[] prods = new int[l];
+        for (int i = 0, prod = 1; i < l; i++) {
+            prods[i] = prod;
+            prod *= nums[i];
+        }
+        for (int i = l - 1, prod = 1; i >= 0; i--) {
+            prods[i] *= prod;
+            prod *= nums[i];
+        }
+        return prods;
+    }
+}
